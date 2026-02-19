d2bb91c4c3d86eb08a6b9011c7d7d9985fa78f08
PhysicistLeon <physicistleon@gmail.com>
2026-02-19 03:31:21 +0300

    Limit click sounds to BigBtn and add runtime sound folder import

--- ui/PasswordSetup.gd
+++ ui/PasswordSetup.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 	$VBox/SaveBtn.pressed.connect(_save)
 
 func _save() -> void:
-	GameController.play_ui_click()
 	var a: String = pass1.text
 	var b: String = pass2.text
 	if a.length() < 3:
